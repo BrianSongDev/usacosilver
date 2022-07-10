7aa4d1c3c7c68ad65395297e0d66b9ec7def3ae3
Brian S <brians3476@gmail.com>
2022-07-09 20:46:31 -0700

    Update Lifeguards.cpp

--- CustomComparators/Lifeguards.cpp
+++ CustomComparators/Lifeguards.cpp
@@ -1,4 +1,4 @@
-// 8 AC, 2 TLE. Jan 2018 Lifeguards (#1).
+// Jan 2018 #1 (Lifeguards)
 #include <iostream>
 #include <string>
 #include <vector>
@@ -36,7 +36,7 @@ int main() {
 		arrToBeUsedLater.pub(mkp(a, b));
 	}
 	sort(shifts.begin(), shifts.end());
-	vector <pair<pair<int, int>, int> > tlOfShifts; int totalTimeWorking = 0;
+	vector <pair<pair<int, int>, int> > tlOfShifts, prefixTimes; int totalTimeWorking = 0;
 	mi indicesOfTimes;
 	tlOfShifts.pub(mkp(mkp(shifts[0].first, shifts[1].first), shifts[0].second));
 	for (int i = 1; i < shifts.size()-1; i++) {
@@ -46,19 +46,30 @@ int main() {
 	tlOfShifts.pub(mkp(mkp(shifts[shifts.size() - 1].first, shifts[shifts.size() - 1].first), 0));
 	indicesOfTimes[shifts[shifts.size() - 1].first] = tlOfShifts.size()-1;
 
+	prefixTimes.pub(mkp(mkp(0, 0), 0));
 	for (auto i : tlOfShifts) {
 		if (i.second > 0) {
 			totalTimeWorking += (i.first.second - i.first.first);
 		}
+		if (i.second == 1) {
+			prefixTimes.pub(mkp(mkp(i.first.first, i.first.second), prefixTimes[prefixTimes.size() - 1].second + i.first.second - i.first.first));
+		}
+		else {
+			prefixTimes.pub(mkp(mkp(i.first.first, i.first.second), prefixTimes[prefixTimes.size() - 1].second));
+		}
 	}
+	//for (auto i : prefixTimes) {
+	//	cout << i.first.first << " " << i.first.second << "    " << i.second << endl;
+	//}
 	int minTimeLost = 0;
 	for (auto i : arrToBeUsedLater) {
 		int timeLost = totalTimeWorking;
-		for (int a = indicesOfTimes[i.first]; a < indicesOfTimes[i.second]; a++) {
-			if (tlOfShifts[a].second == 1) {
-				timeLost -= tlOfShifts[a].first.second - tlOfShifts[a].first.first;
-			}
-		}
+		//for (int a = indicesOfTimes[i.first]; a < indicesOfTimes[i.second]; a++) {
+		//	if (tlOfShifts[a].second == 1) {
+		//		timeLost -= tlOfShifts[a].first.second - tlOfShifts[a].first.first;
+		//	}
+		//}
+		timeLost -= prefixTimes[indicesOfTimes[i.second]].second - prefixTimes[indicesOfTimes[i.first]].second;
 		minTimeLost = max(minTimeLost, timeLost);
 	}
 	cout << minTimeLost << endl;
